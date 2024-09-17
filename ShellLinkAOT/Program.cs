30fd7ca26d59a98cc8f5cee497e8e3d98cf737d1
MakesYT <2696703792@qq.com>
2024-09-17 16:17:14 +0800

    1

---
+++ ShellLinkAOT/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Vanara.Windows.Shell;
+
+var shellLink = ShellLink.Create(
+    $"{Environment.GetFolderPath(Environment.SpecialFolder.Programs)}\\1.lnk",
+    $"{Environment.GetFolderPath(Environment.SpecialFolder.Programs)}\\1.lnk",null);
+Console.WriteLine("Hello, World!");
